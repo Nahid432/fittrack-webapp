33128060b9bfaf71299d00e2cd2e492641b2bddf
Nahidul Hoque <nahidul.hoque@outlook.com>
2026-07-29 01:33:37 +0100

    Implemented exercise search and filtering

--- routes/exercises.js
+++ routes/exercises.js
@@ -5,14 +5,36 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/list', function(req,res, next) {
-  const sqlQuery = "SELECT * FROM exercises"
 
-  db.query(sqlQuery, function(err, results) {
+  // Retrieve values from the URL query string
+  const name = req.query.exercise_name || ""
+  const muscleGroup = req.query.muscle_group || ""
+
+  let sqlQuery = "SELECT * FROM exercises WHERE 1 = 1"
+
+  const queryValues = []
+
+  // Search by exercise name
+  if (name !== "") {
+    sqlQuery += " AND name LIKE ?"
+    queryValues.push("%" + name + "%")
+  }
+
+  // Filter by muscle group
+  if (muscleGroup !== "") {
+    sqlQuery += " AND muscle_group = ?"
+    queryValues.push(muscleGroup)
+  }
+
+  db.query(sqlQuery, queryValues, function(err, results) {
     if (err) {
         return next(err)
     }
 
-    res.render("exercise_list.ejs", {availableExercises: results})
+    res.render("exercise_list.ejs", {
+      availableExercises: results,
+      exerciseName: name,
+    })
   })
 })
 
